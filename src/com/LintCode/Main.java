eef303b85849813c16d8b3f5565ca077efed0260
kf953741 <761551096@qq.com>
2016-01-19 18:34:39 +0800

    AC LC62

--- src/com/LintCode/Main.java
+++ src/com/LintCode/Main.java
@@ -6,7 +6,8 @@ public class Main {
     {
         //System.out.println(LC16.strStr("abc","bc"));
         //System.out.println(LC14.binarySearch(new int[]{3,4,5,8,8,8,8,10,13,14},8));
-        System.out.println(LC75.findPeak(new int[]{1,2,1,3,4,5,7,6}));
+        //System.out.println(LC75.findPeak(new int[]{1,2,1,3,4,5,7,6}));
+        System.out.print(LC62.search(new int[]{4,5,1,2,3},1));
     }
 
 
